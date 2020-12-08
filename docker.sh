d5d224532a66dc1fe9119c8e85ca2e2e3a4be5d7
bekzhan <bekzhan@192.168.13.1>
2020-12-08 18:20:04 +0600

    fix docker, and etc bugs

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
     #!/bin/bash
-    docker build --tag forum .
-    docker run --publish 6969:6969 --detach --name forumxx forum
+    docker build --tag forumx .
+    docker run --publish 6969:6969 --detach --name forumx forum
